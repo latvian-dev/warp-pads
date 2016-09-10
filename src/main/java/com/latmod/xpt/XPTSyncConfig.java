81368c6dffa572df20db90e7fcc898e8c5a5088f
LatvianModder <latvianmodder@gmail.com>
2016-09-11 00:04:02 +0300

    Added ISyncData

--- src/main/java/com/latmod/xpt/XPTSyncConfig.java
+++ src/main/java/com/latmod/xpt/XPTSyncConfig.java
@@ -1,15 +1,17 @@
 package com.latmod.xpt;
 
+import com.feed_the_beast.ftbl.api.IForgePlayer;
+import com.feed_the_beast.ftbl.api.ISyncData;
+import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.nbt.NBTTagCompound;
-import net.minecraftforge.common.util.INBTSerializable;
 
 /**
  * Created by LatvianModder on 17.08.2016.
  */
-public class XPTSyncConfig implements INBTSerializable<NBTTagCompound>
+public class XPTSyncConfig implements ISyncData
 {
     @Override
-    public NBTTagCompound serializeNBT()
+    public NBTTagCompound writeSyncData(EntityPlayerMP player, IForgePlayer forgePlayer)
     {
         NBTTagCompound nbt = new NBTTagCompound();
         nbt.setBoolean("SB", XPTConfig.soft_blocks.getAsBoolean());
@@ -17,7 +19,7 @@ public class XPTSyncConfig implements INBTSerializable<NBTTagCompound>
     }
 
     @Override
-    public void deserializeNBT(NBTTagCompound nbt)
+    public void readSyncData(NBTTagCompound nbt)
     {
         XPTConfig.soft_blocks.set(nbt.getBoolean("SB"));
     }
