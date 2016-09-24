f96ce171ad5953e3d68f9c3d74ace8ad1068a71b
LatvianModder <latvianmodder@gmail.com>
2016-09-25 00:47:16 +0300

    Updated to new FTBLib registries

--- src/main/java/com/latmod/xpt/XPTItems.java
+++ src/main/java/com/latmod/xpt/XPTItems.java
@@ -1,8 +1,14 @@
 package com.latmod.xpt;
 
+import com.feed_the_beast.ftbl.api.item.ODItems;
+import com.feed_the_beast.ftbl.api.recipes.IRecipeHandler;
+import com.feed_the_beast.ftbl.api.recipes.IRecipes;
+import com.feed_the_beast.ftbl.api.recipes.RecipeHandler;
 import com.latmod.lib.util.LMUtils;
 import com.latmod.xpt.block.BlockTeleporter;
 import com.latmod.xpt.block.TileTeleporter;
+import net.minecraft.init.Blocks;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
 
 /**
@@ -10,10 +16,38 @@ import net.minecraft.util.ResourceLocation;
  */
 public class XPTItems
 {
-    public static final BlockTeleporter TELEPORTER = LMUtils.register(new ResourceLocation(XPT.MOD_ID, "teleporter"), new BlockTeleporter());
+    public static final BlockTeleporter TELEPORTER = new BlockTeleporter();
 
     public static void init()
     {
+        LMUtils.register(new ResourceLocation(XPT.MOD_ID, "teleporter"), TELEPORTER);
         LMUtils.addTile(TileTeleporter.class, TELEPORTER.getRegistryName());
     }
+
+    @RecipeHandler
+    public static final IRecipeHandler RECIPES = new IRecipeHandler()
+    {
+        @Override
+        public ResourceLocation getID()
+        {
+            return new ResourceLocation(XPT.MOD_ID, "items");
+        }
+
+        @Override
+        public boolean isActive()
+        {
+            return XPTConfig.ENABLE_CRAFTING.getBoolean();
+        }
+
+        @Override
+        public void loadRecipes(IRecipes recipes)
+        {
+            recipes.addRecipe(new ItemStack(XPTItems.TELEPORTER),
+                    " G ", "DPD", "III",
+                    'I', Blocks.HEAVY_WEIGHTED_PRESSURE_PLATE,
+                    'P', ODItems.ENDERPEARL,
+                    'G', ODItems.GLOWSTONE,
+                    'D', ODItems.DIAMOND);
+        }
+    };
 }
--- src/main/java/com/latmod/xpt/XPTSyncConfig.java
+++ src/main/java/com/latmod/xpt/XPTSyncConfig.java
@@ -2,14 +2,25 @@ package com.latmod.xpt;
 
 import com.feed_the_beast.ftbl.api.IForgePlayer;
 import com.feed_the_beast.ftbl.api.ISyncData;
+import com.feed_the_beast.ftbl.api.SyncData;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.util.ResourceLocation;
 
 /**
  * Created by LatvianModder on 17.08.2016.
  */
-public class XPTSyncConfig implements ISyncData
+public enum XPTSyncConfig implements ISyncData
 {
+    @SyncData
+    INSTANCE;
+
+    @Override
+    public ResourceLocation getID()
+    {
+        return new ResourceLocation(XPT.MOD_ID, "config");
+    }
+
     @Override
     public NBTTagCompound writeSyncData(EntityPlayerMP player, IForgePlayer forgePlayer)
     {
